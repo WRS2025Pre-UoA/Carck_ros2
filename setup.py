eefad6490c1ef330c55811f3fd3a9c61945f5694
rui623 <ruiiwata837@gmail.com>
2024-09-20 16:11:29 +0900

    OK

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'crack'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ros',
+    maintainer_email='ruiiwata837@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = crack.test_publisher:main',
+            'image_publisher = crack.image_publisher:main',  # パッケージ名を小文字に変更
+            'image_subscriber = crack.image_subscriber:main',  # パッケージ名を小文字に変更
+        ],
+    },
+)
